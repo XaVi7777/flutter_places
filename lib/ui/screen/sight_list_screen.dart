eca2abdad4cdd5733709fad85777075152c0e370
XaVi7777 <is.kursakoff@yandex.rum>
2021-08-20 10:17:33 +0300

    final

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+const double _appBarHeight = 72;
+const double _appBarTopPadding = 64;
+const double _appBarElevation = 0;
+const Padding _appBarTitleText = Padding(
+    padding: EdgeInsets.only(top: _appBarTopPadding),
+    child: Text(
+      'Список\nинтересных мест',
+      style: TextStyle(
+        color: Color.fromRGBO(37, 40, 73, 1),
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w700,
+        fontSize: 32,
+      ),
+      textAlign: TextAlign.left,
+    ));
+
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
 
@@ -10,8 +26,14 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _appBarHeight + _appBarTopPadding,
+        elevation: _appBarElevation,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: _appBarTitleText,
+      ),
+      body: const Center(
         child: Text('Hello'),
       ),
     );
